14a02fece04378732d9ee7c7a3ee0e2344613017
Mohammad Al-Ahdal <mo@mohammad.dev>
2019-08-26 15:10:00 -0600

    add index.ts base

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+require('dotenv').config();
+import { Db, MongoError, MongoClient } from "mongodb";
+import express from 'express'
+import moment from 'moment'
+import bodyParser from 'body-parser'
+import axios from 'axios'
+
+const mongocs = process.env.MONGO_CONNECTION_STRING;
+let db: Db;
+const port = process.env.PORT ? process.env.PORT : 6003;
+const app = express();
+
+app.use(bodyParser.json());
+
+require('mongodb').connect(mongocs, (err: MongoError, result: MongoClient) => {
+    if (err) {
+        console.log(err)
+        process.exit(1);
+    } else {
+        db = result.db('default')
+    }
+})
+
+const checkTokenAuthenticatedWithAuthServer = async (token: String) => {
+    let status = (await axios.post(`/api/auth/authenticateUsingToken`, { token })).data.code
+    return status === 200 ? true : false
+}
+
+app.listen(port, () => console.log(`Photography microservice listening on port: ${port}!`))
